3d431ee8adc915ef5d8aac60bccd3b0ce42e5711
Dileepa <bmdbalasuriya@gmail.com>
2017-05-12 11:56:29 +0530

    initial commits

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,11 +1,11 @@
 module.exports = {
-  mongodb: {
-    host: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST',
-    port: '$OPENSHIFT_MONGODB_DB_PORT',
-    database: 'deezdb',
-    username: 'OPENSHIFT_MONGODB_DB_USERNAME',
-    password: 'OPENSHIFT_MONGODB_DB_PASSWORD',
-    name: 'mongodb',
-    connector: 'mongodb'
-  }
+  // mongodb: {
+  //   host: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST',
+  //   port: '$OPENSHIFT_MONGODB_DB_PORT',
+  //   database: 'deezdb',
+  //   username: 'OPENSHIFT_MONGODB_DB_USERNAME',
+  //   password: 'OPENSHIFT_MONGODB_DB_PASSWORD',
+  //   name: 'mongodb',
+  //   connector: 'mongodb'
+  // }
 };
--- server/server.js
+++ server/server.js
@@ -18,6 +18,8 @@ app.start = function() {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
+
+    console.log('$OPENSHIFT_MONGODB_DB_HOST');
   });
   return server;
 };
